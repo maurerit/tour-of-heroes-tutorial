93ca82ce52a9b2124dbbf08a059ac259e0ac64d7
Matt Maurer <maurer.it@gmail.com>
2017-09-10 22:19:17 -0400

    https://angular.io/tutorial/toh-pt6 completed
    Removed the global style sheet's reference from the index.html.  The browser was complaining that the file didn't exist and while I figured angular-cli included it I wasn't 100% sure.  This just confirmed it.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule} from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+// Imports for loading and configuring the in-memory web api
+import { InMemoryWebApiModule} from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
@@ -8,17 +13,21 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroService } from './hero.service';
 import { AppRoutingModule } from './app-routing.module';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroDetailComponent,
     HeroesComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeroSearchComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
   ],
   providers: [HeroService],
